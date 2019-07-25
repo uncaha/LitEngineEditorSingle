ae5329886415f651e435fc61f3f9fa6bc85811f9
zhanying.yang <zhanying.yang@mobvista.com>
2019-07-25 18:17:40 +0800

    add xlsx

--- LitEngineEditor/Script/Windows/ExportExcelWiindow.cs
+++ LitEngineEditor/Script/Windows/ExportExcelWiindow.cs
@@ -12,7 +12,7 @@ namespace LitEngineEditor
     {
         private Vector2 mScrollPosition = Vector2.zero;
         private StringBuilder mContext = new StringBuilder();
-        protected string filestag = "*.xls";
+        protected string filestag = "*.xls|*.xlsx";
         public ExportExcelWiindow() : base()
         {
             ExWType = ExportWType.ExcelWindow;
