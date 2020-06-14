d70496080afd148243979b24ece4d7f04518f6ed
uncaha <cczxntken@163.com>
2020-06-14 13:05:16 +0800

    Update ExportToData.cs

--- LitEngineEditor/Script/Excel/ExportToData.cs
+++ LitEngineEditor/Script/Excel/ExportToData.cs
@@ -9,7 +9,7 @@ namespace ExportTool
         string tempFile;
         ExcelData data;
 
-        public ExportToData(string pName,ExcelData pData)
+        public ExportToData(string pName, ExcelData pData)
         {
             filename = pName;
             data = pData;
@@ -21,11 +21,23 @@ namespace ExportTool
         {
             try
             {
+                DLog.Log("开始导出:" + tempFile);
                 FileStream tfile = File.OpenWrite(tempFile);
                 BinaryWriter twt = new BinaryWriter(tfile);
-                twt.Write(data.r - ExcelData.sStartLine);
+                int tline = data.r - ExcelData.sStartLine;
+                twt.Write(tline);
+                DLog.LogFormat("共有 {0} 行数据",tline);
                 for (int i = ExcelData.sStartLine; i < data.r; i++)
                 {
+                    if(string.IsNullOrEmpty(data.objects[i, 0]))
+                    {
+                        twt.Flush();
+                        twt.Close();
+                        tfile.Close();
+                        File.Delete(tempFile);
+                        ShowError(i, 0,"配置表第一列id不能为空,请检查配置表.是否有空列,并删除.");
+                        return;
+                    }
                     for (int j = 0; j < data.c; j++)
                     {
                         if (!data.IsNeed(j)) continue;
@@ -36,10 +48,7 @@ namespace ExportTool
                             twt.Close();
                             tfile.Close();
                             File.Delete(tempFile);
-                            if (UnityEditor.EditorUtility.DisplayDialog("Error", $"表 {filename} 生成配置出现错误第{i}行,第{j}列.erro = {terro.ToString()}", "ok"))
-                            {
-                                DLog.LogError($"表 {filename} 生成配置出现错误第{i}行,第{j}列.erro = {terro.ToString()}");
-                            }
+                            ShowError(i,j,terro.Message);
                             return;
                         }
                     }
@@ -53,9 +62,17 @@ namespace ExportTool
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
+                DLog.LogError(ex);
             }
-          
+
+        }
+
+        void ShowError(int i,int j,string pErro)
+        {
+            if (UnityEditor.EditorUtility.DisplayDialog("Error", $"表 {filename} 生成配置出现错误第{i}行,第{j}列.erro = {pErro}", "ok"))
+            {
+                DLog.LogError($"表 {filename} 生成配置出现错误第{i}行,第{j}列.erro = {pErro}");
+            }
         }
 
         public System.Exception WriteData(BinaryWriter _write, string _typestr, string _value)
@@ -94,6 +111,12 @@ namespace ExportTool
         }
         protected void WriteValue(BinaryWriter _write, string _typestr, string _value)
         {
+            if (string.IsNullOrEmpty(_value))
+            {
+                WriteNullValue(_write, _typestr, _value);
+                return;
+            }
+
             switch (_typestr)
             {
                 case "int":
@@ -103,17 +126,11 @@ namespace ExportTool
                     _write.Write(float.Parse(_value));
                     break;
                 case "string":
-                    if (string.IsNullOrEmpty(_value))
-                    {
-                        _write.Write((int)0);
-                    }
-                    else
                     {
                         byte[] tbytes = System.Text.UTF8Encoding.UTF8.GetBytes(_value);
                         _write.Write(tbytes.Length);
                         _write.Write(tbytes);
                     }
-
                     break;
                 case "long":
                     _write.Write(long.Parse(_value));
@@ -126,6 +143,34 @@ namespace ExportTool
                     break;
                 case "bool":
                     _write.Write(bool.Parse(_value));
+                    break;
+            }
+        }
+
+        protected void WriteNullValue(BinaryWriter _write, string _typestr, string _value)
+        {
+            switch (_typestr)
+            {
+                case "int":
+                    _write.Write((int)0);
+                    break;
+                case "float":
+                    _write.Write((float)0);
+                    break;
+                case "string":
+                    _write.Write((int)0);
+                    break;
+                case "long":
+                    _write.Write((long)0);
+                    break;
+                case "byte":
+                    _write.Write((byte)0);
+                    break;
+                case "short":
+                    _write.Write((short)0);
+                    break;
+                case "bool":
+                    _write.Write(false);
                     break;
             }
         }
