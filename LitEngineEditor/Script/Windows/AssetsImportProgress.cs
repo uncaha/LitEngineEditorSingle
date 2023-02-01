517986976e7a1a179149959eb425cf5acb6b418f
uncaha <yangzhanying@habby.com>
2023-02-01 20:14:51 +0800

    Update AssetsImportProgress.cs

--- LitEngineEditor/Script/Windows/AssetsImportProgress.cs
+++ LitEngineEditor/Script/Windows/AssetsImportProgress.cs
@@ -5,16 +5,16 @@ public class AssetsImportProgress : AssetPostprocessor
     public void OnPostprocessModel(GameObject _model)
     {
        /// ModelImporter modelImp = (ModelImporter)assetImporter;
-        Renderer[] trenders = _model.GetComponentsInChildren<Renderer>();
-        for (int i = 0; i < trenders.Length; i++)
-        {
-            trenders[i].sharedMaterial = null;
-
-            if (trenders[i].sharedMaterials != null)
-            {
-                trenders[i].sharedMaterials = new Material[0];
-            }
-        }
+        // Renderer[] trenders = _model.GetComponentsInChildren<Renderer>();
+        // for (int i = 0; i < trenders.Length; i++)
+        // {
+        //     trenders[i].sharedMaterial = null;
+        //
+        //     if (trenders[i].sharedMaterials != null)
+        //     {
+        //         trenders[i].sharedMaterials = new Material[0];
+        //     }
+        // }
     }
 
     public void OnPostprocessTexture(Texture2D _tex)
